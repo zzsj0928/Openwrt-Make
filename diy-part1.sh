b7d47ca72923a7f035eb4520194b0f78792fee8b
然后七年 <34766756+xylz0928@users.noreply.github.com>
2020-08-28 10:57:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i -c -e '/^#/d' feeds.conf.default
+sed -i -e '/^#/d' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
