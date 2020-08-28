2ee11d7f7b1c41e463e575861f971160ace76d94
然后七年 <34766756+xylz0928@users.noreply.github.com>
2020-08-28 10:46:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,6 +15,9 @@
 sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
 
 # Add ServerChan
-git clone https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+# git clone https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+
 # Add Onliner
-git clone https://github.com/rufengsuixing/luci-app-onliner.git feeds/luci/applications/luci-app-onliner
+# git clone https://github.com/rufengsuixing/luci-app-onliner.git feeds/luci/applications/luci-app-onliner
+git clone https://github.com/rufengsuixing/luci-app-onliner.git package/luci-app-onliner
